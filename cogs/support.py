f42656254e925bb1c337a8008812902daf3e393f
David Senpai <51710965+DavidSenpai@users.noreply.github.com>
2022-02-08 20:28:44 +0100

    Create support.py
    assistance module for providing python support on the server

---
+++ cogs/support.py
@@ -0,0 +1,18 @@
+import nextcord
+
+class Support(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.group(case_insensitive=True)
+    @commands.guild_only()
+    async def tag(self,ctx):
+      if (ctx.invoked_subcommand is None): None
+  
+    @tag.command()
+    @commands.guild_only()
+    async def create(self,ctx,name):
+      await ctx.send("No") #Adding this soon
+
+def setup(bot):
+    bot.add_cog(Support(bot))

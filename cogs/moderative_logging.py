3bcec86b7a971f71ca6b6bfc1493748966ce8e93
Scrumpy <scrumpy@weeb.email>
2022-02-08 19:08:17 +0000

    Setting up inital bot + errors & moderative logs

---
+++ cogs/moderative_logging.py
@@ -0,0 +1,75 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from nextcord.ext import commands
+from nextcord import (
+    Embed,
+    Colour
+)
+
+class ModerativeLogging(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.message_log_channel_id = int(os.getenv('MESSAGE_LOG_CHANNEL_ID'))
+
+    def formatList(self, needs_formatting):
+        format_list = ['{:>3}' for item in needs_formatting] 
+        s = '\n'.join(format_list)
+        return s.format(*needs_formatting)
+
+    def createMessageDeleteLogEmbed(self, message, bulk=False):
+        message_log_embed = Embed(
+            colour=Colour.dark_red() if bulk else Colour.red(),
+            title=f"A message sent by '{message.author.name}' was deleted.",
+            description=message.content
+        )
+        message_log_embed.set_footer(text=f"User ID: {message.author.id} | Message ID: {message.id}")
+        if len(message.attachments) > 0:
+            message_log_embed.add_field(
+                name="Attachments", 
+                value=self.formatList([f"[**{att.content_type.upper()}** (*{att.filename})]({att.url}*)" for att in message.attachments])
+            )
+        return message_log_embed
+
+    def createMessageEditLogEmbed(self, old_message, new_message):
+        message_log_embed = Embed(
+            colour=Colour.yellow(),
+            title=f"'{old_message.author.name}' has edited their message."
+        )
+        message_log_embed.set_footer(text=f"User ID: {old_message.author.id} | Message ID: {old_message.id}")
+        message_log_embed.add_field(name="Before", value=old_message.content, inline=False)
+        message_log_embed.add_field(name="After", value=new_message.content, inline=False)
+        message_log_embed.add_field(name="Information", value=f"**[Click me to jump to the message]({old_message.jump_url})** | Sent in {old_message.channel.mention}", inline=False)
+        return message_log_embed
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        if not message.author.bot and "ml-exempt" not in str(message.channel.topic):
+            message_log_channel = self.bot.get_channel(self.message_log_channel_id)
+            if message_log_channel is not None:
+                message_log_embed = self.createMessageDeleteLogEmbed(message)
+                await message_log_channel.send(embed=message_log_embed)
+
+    @commands.Cog.listener()
+    async def on_bulk_message_delete(self, messages):
+        embeds_list = []
+        for message in messages:
+            if not message.author.bot and "ml-exempt" not in str(message.channel.topic):
+                embeds_list.append(self.createMessageDeleteLogEmbed(message, bulk=True))
+        message_log_channel = self.bot.get_channel(self.message_log_channel_id)
+        if message_log_channel is not None:
+            log_segments = [embeds_list[x:x+20] for x in range(0, len(embeds_list), 20)] # Splitting up the embeds_list into multiple seperate lists each being 20 messages long
+            for log_segment in log_segments:
+                await message_log_channel.send(embeds=log_segment) # Sending up to 20 messages (a log segment) per message, to reduce overal log messages sent
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, old_message, new_message):
+        if not old_message.author.bot and "ml-exempt" not in str(old_message.channel.topic):
+            message_log_channel = self.bot.get_channel(self.message_log_channel_id)
+            if message_log_channel is not None:
+                message_log_embed = self.createMessageEditLogEmbed(old_message, new_message)
+                await message_log_channel.send(embed=message_log_embed)
+
+def setup(bot):
+    bot.add_cog(ModerativeLogging(bot))
---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from nextcord.ext.commands import Bot
+from nextcord.ext import commands
+from nextcord import (
+    AllowedMentions,
+    Intents,
+    Status,
+    Activity,
+    MemberCacheFlags
+)
+
+import asyncio
+import logging
+
+logger = logging.basicConfig(
+    format='[%(asctime)s] %(process)d-%(levelname)s : %(message)s', 
+    datefmt='%d-%b-%y %H:%M:%S', 
+    filename='zoey-errors.log', 
+    level=logging.WARNING
+)
+
+intents = Intents.none()
+intents.guilds = True
+intents.invites = True
+intents.members = True
+intents.voice_states = True
+intents.guild_messages = True
+intents.guild_reactions = True
+
+bot = commands.Bot(
+    status=Status.idle, 
+    activity=Activity(name=f'over the table.', type=3),
+    allowed_mentions=AllowedMentions(everyone=False, users=True, roles=False),
+    member_cache_flags=MemberCacheFlags(voice=False, joined=False),
+    command_prefix="-", 
+    case_insensitive=True,
+    chunk_guilds_at_startup=False,
+    strip_after_prefix=True,
+    help_command=None,
+    max_messages=2500,
+    intents=intents,
+    heartbeat_timeout=20.0
+)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.message.add_reaction("⏰")
+        return await ctx.reply(f"`⏰` You're on cooldown. | Try again in **{int(error.retry_after)}s**.", delete_after=error.retry_after if error.retry_after <= 60 else None)
+    elif isinstance(error, commands.MissingPermissions):
+        return await ctx.reply(f"`🧦` You're lacking the following permission `{list(error.missing_perms)[0]}`.")
+    elif isinstance(error, errors.MissingRequiredArgument):
+        return await ctx.reply("`🚨` You're missing a required argument!")
+    elif isinstance(error, errors.TooManyArguments):
+        return await ctx.reply("`🚨` You've given me too many arguments!")
+    elif isinstance(error, errors.BadArgument):
+        return await ctx.reply("`🚨` I'm not sure what one of your arguments is!")
+    elif not isinstance(error, errors.CommandNotFound):
+        return logging.warning(error)
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+    elif os.path.isfile(filename):
+        logging.warning(f"Unable to load {filename[:-3]}")
+
+bot.run(os.getenv('TOKEN'))
